9e5502cebb023de925e091f354bb84d562f1a772
lev david <levdavid1@gmail.com>
2016-03-21 18:19:31 -0400

    created csv with top10 words, if any

--- extract_keywords.py
+++ extract_keywords.py
@@ -1,17 +1,24 @@
 import rake
 import operator
 import json
+import csv
 
 stoppath = "RAKE-tutorial/FoxStoplist.txt"
 
-rake_obj = rake.Rake(stoppath, 5, 3, 1)
+rake_obj = rake.Rake(stoppath, 4, 4, 1)
 
 input_file = open("data/1.json", 'r')
 
+output_file = open("data/1.csv", 'wb')
+
 json_decode = json.load(input_file)
 
-text = json_decode['0'].get('comments')[0]['text']
+comments = json_decode['0'].get('comments')
 
-keywords = rake_obj.run(text)
+writer = csv.writer(output_file, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
 
-print("Keywords:", keywords)
+for comment in comments:
+	print comment['text']
+	keywords = rake_obj.run(comment['text'])
+	print("Keywords:", keywords[0:10])
+	writer.writerow([comment['text'],keywords[0:10]])
